34ee9d04f369c1e7d7ea35b8964a0971404a5b9b
stwn11 <54468838+stwn11@users.noreply.github.com>
2020-12-15 00:33:57 +0800

    Initial commit

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Sayang", "BEB", "Akan", "Di", "Random"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
